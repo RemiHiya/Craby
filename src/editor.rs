c5de8ed954c7c3fb43d3fecb9003e1aed084a9f6
reschirra <remi.schirra@etu.uca.fr>
2024-06-08 14:16:08 +0200

    Status line

--- src/editor.rs
+++ src/editor.rs
@@ -1,6 +1,7 @@
 use std::io::{stdout, Stdout, Write};
 use crossterm::{terminal, ExecutableCommand, QueueableCommand, cursor, event, style};
 use crossterm::event::{read};
+use crossterm::style::{Color, Stylize};
 
 enum Action {
     Quit,
@@ -15,6 +16,7 @@ enum Action {
     EnterMode(Mode)
 }
 
+#[derive(Debug)]
 enum Mode {
     Normal,
     Insert
@@ -59,8 +61,33 @@ impl Editor {
     }
 
     pub fn draw_statusline(&mut self) -> anyhow::Result<()> {
+        let mode = format!(" {:?} ", self.mode).to_uppercase();
+        let file = " src/main.rs";
+        let pos = format!(" {}:{} ", self.cx, self.cy);
+
+        let file_width = self.size.0 - mode.len() as u16 - pos.len() as u16 - 2;
+
+        let normal_bg = Color::Rgb {r:184, g:144, b:243};
+        let insert_bg = 1;
+        let classic_bg = Color::Rgb {r:67, g:70, b:89};
+
         self.stdout.queue(cursor::MoveTo(0, self.size.1 - 2))?;
-        self.stdout.queue(style::Print("Status line"))?;
+        self.stdout.queue(style::PrintStyledContent(
+            mode.with(Color::Black).bold().on(normal_bg)
+        ))?;
+        self.stdout.queue(style::PrintStyledContent("".with(normal_bg).on(classic_bg)))?;
+        self.stdout.queue(style::PrintStyledContent(
+            format!("{:<width$}", file, width = file_width as usize)
+                .with(Color::White)
+                .bold()
+                .on(classic_bg),
+        ))?;
+        self.stdout.queue(style::PrintStyledContent(
+           "".with(normal_bg).on(classic_bg)
+        ))?;
+        self.stdout.queue(style::PrintStyledContent(
+            pos.with(Color::Black).on(normal_bg)
+        ))?;
         Ok(())
     }
 
@@ -92,6 +119,9 @@ impl Editor {
 
 
     fn handle_event(&mut self, ev: event::Event) -> anyhow::Result<Option<Action>>{
+        // if matches!(ev, event::Event::Resize(_, _)) {
+        //     self.size = terminal::size()?;
+        // }
         match self.mode {
             Mode::Normal => self.handle_normal_event(ev),
             Mode::Insert => self.handle_insert_event(ev),
